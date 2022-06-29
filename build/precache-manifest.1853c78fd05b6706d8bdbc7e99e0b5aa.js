de1bdda36f931a53ba3f1dc74500e172f7b8acdf
DebugSp07 <91491981+DebugSp07@users.noreply.github.com>
2022-06-29 12:33:15 -0700

    Add files via upload

---
+++ build/precache-manifest.1853c78fd05b6706d8bdbc7e99e0b5aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8144322259c56d9716eb",
+    "url": "/textBuddy/static/css/main.0c70b30a.chunk.css"
+  },
+  {
+    "revision": "8144322259c56d9716eb",
+    "url": "/textBuddy/static/js/main.81443222.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/textBuddy/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "109cecdacbf245e06942",
+    "url": "/textBuddy/static/js/2.109cecda.chunk.js"
+  },
+  {
+    "revision": "65a51694aa75c6454802",
+    "url": "/textBuddy/static/js/runtime~main.65a51694.js"
+  },
+  {
+    "revision": "2f5bc37218e3dd818fcfb6f5e31bff24",
+    "url": "/textBuddy/index.html"
+  }
+];
